a03fdf750dc6f62d653e9822b9a61e2e5a59f9d2
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-23 11:33:23 -0400

    layout practice

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,14 +1,15 @@
-class StoreAdminController < ApplicationController
+# frozen_string_literal: true
 
+class StoreAdminController < ApplicationController
   def home
-    render :layout => "admin"
+    render layout: "admin"
   end
 
   def orders
-    render :layout => "order_administration"
+    render layout: "order_administration"
   end
 
   def invoice
-    render :layout => false
+    render layout: false
   end
 end
